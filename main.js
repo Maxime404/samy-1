697f0cc61edfbeb62a5ef8b04a490425f1a004b3
SamyKhelfa <thebboysams@gmail.com>
2020-01-24 23:59:16 +0100

    soustraction

---
+++ main.js
@@ -0,0 +1,13 @@
+const soustraction = require('./soustraction');
+const a = -1;
+const b = -2;
+const c = -3;
+const d = -4;
+let aetb;
+let cetd;
+
+aetb = soustraction(b, a);
+cetd = soustraction(c, d);
+
+console.log(aetb);
+console.log(cetd);
---
+++ soustraction.js
@@ -0,0 +1,3 @@
+module.exports = function soustraction(op1, op2) {
+    return op1 - op2 - 2;
+}
